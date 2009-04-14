a5f89cc18c05be95c441377f8d55d5743372e4b7
lesterfd@gmail.com <lesterfd@gmail.com@0f5e3838-05e2-11de-9c5c-7db7966a26d7>
2009-04-14 03:31:54 +0000

    CVM GUI

---
+++ RRComSSys/Projects/CVM/RRComSSys.CVM/UserInterface/CVMGUI/CVMLogin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using RRComSSys.CVM.UserInterface.Security;
+
+namespace RRComSSys.CVM.UserInterface.CVMUI
+{
+    public partial class CVMLogin : Form
+    {
+        private Login login;
+
+        public CVMLogin()
+        {
+            InitializeComponent();
+            login = new Login();
+        }
+
+        private void LoginBtn_Click(object sender, EventArgs e)
+        {
+            if (!login.CheckCredentials(tbUsername.Text, tbPassword.Text))
+                MessageBox.Show("Invalid credentials. Login failed!");
+            else
+            {
+                tbPassword.Clear();
+                tbUsername.Clear();
+                this.DialogResult = DialogResult.OK;
+            }
+        }
+    }
+}
---
+++ RRComSSys/Projects/CVM/RRComSSys.CVM/UserInterface/CVMGUI/CVMMain.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RRComSSys.CVM.UserInterface.CVMUI
+{
+    public partial class CVMMain : Form
+    {
+        private int childFormNumber = 0;
+        private CVMLogin CVMLogin;
+
+        public CVMMain()
+        {
+            InitializeComponent();
+            CVMLogin = new CVMLogin();
+        }
+
+        private void ShowNewForm(object sender, EventArgs e)
+        {
+            Form childForm = new Form();
+            childForm.MdiParent = this;
+            childForm.Text = "Window " + childFormNumber++;
+            childForm.Show();
+        }
+
+        private void OpenFile(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            openFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            if (openFileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                string FileName = openFileDialog.FileName;
+            }
+        }
+
+        private void SaveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            saveFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            if (saveFileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                string FileName = saveFileDialog.FileName;
+            }
+        }
+
+        private void ExitToolsStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void CopyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void PasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void ToolBarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolStrip.Visible = toolBarToolStripMenuItem.Checked;
+        }
+
+        private void StatusBarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            statusStrip.Visible = statusBarToolStripMenuItem.Checked;
+        }
+
+        private void CascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void TileVerticalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void TileHorizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void ArrangeIconsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.ArrangeIcons);
+        }
+
+        private void CloseAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (Form childForm in MdiChildren)
+            {
+                childForm.Close();
+            }
+        }
+
+        private void CVMMain_Shown(object sender, EventArgs e)
+        {
+           if (DialogResult.Cancel == CVMLogin.ShowDialog())
+               this.Close();
+        }
+
+        private void logOffToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.Cancel == CVMLogin.ShowDialog())
+                this.Close();
+        }
+    }
+}
