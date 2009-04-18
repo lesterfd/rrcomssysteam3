5a7eed00b1725c533998699a191a8a8f623d8dea
dflor003 <dflor003@0f5e3838-05e2-11de-9c5c-7db7966a26d7>
2009-04-18 02:06:20 +0000

    Added utilities project for testing

---
+++ RRComSSys/AutomatedTests/MBUnit/RRComSSys.Testing.Common/TestConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RRComSSys.Testing.Common
+{
+	public static class TestConstants
+	{
+		public static class Categories
+		{
+			public const String UnitTest = "Unit Test";
+			public const String IntegrationTest = "Integration Test";
+		}
+	}
+}
