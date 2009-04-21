a16c377e3a97068282514f5a9d1798e2e11340ad
dflor003 <dflor003@0f5e3838-05e2-11de-9c5c-7db7966a26d7>
2009-04-21 00:51:59 +0000

    Fixed tests, added workflow tests

---
+++ trunk/RRComSSys/AutomatedTests/MBUnit/RRComSSys.CVM.ObjectModel.Test/XCMLModel/XCMLWorkflowDocumentTester.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MbUnit.Framework;
+using RRComSSys.CVM.ObjectModel.XCMLWorkflowModel;
+using RRComSSys.Testing.Common;
+
+namespace RRComSSys.CVM.ObjectModel.Test.XCMLModel
+{
+	[TestFixture]
+	[TestCategory(TestConstants.Categories.UnitTest)]
+	[TestsOn(typeof(XCMLWorkflowDocument))]
+	[Author("Danil Mariovich Flores", "Danil.Flores@ultimatesoftware.com")]
+	public class XCMLWorkflowDocumentTester
+	{
+		#region Tests
+		[Test]
+		public void Test_Load_Workflow_Document()
+		{
+			String fileName = @".\TestFiles\sample_wf_complete_1.wfgcml";
+
+			// Load document
+			XCMLWorkflowDocument doc = XCMLWorkflowDocument.LoadDocument<XCMLWorkflowDocument>(fileName);
+
+			// Assert start
+			Assert.AreEqual(true, true);
+
+		}
+		#endregion
+	}
+}
