ea05133292556f50beadfb7d7a9a09d3ae7d4c8f
dflor003 <dflor003@0f5e3838-05e2-11de-9c5c-7db7966a26d7>
2009-04-22 17:59:10 +0000

    Refactored command interfaces and handlers into single files

---
+++ trunk/RRComSSys/Projects/CVM/RRComSSys.CVM.Transformers/SynthesisEngine/CommandInterfaces.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RRComSSys.CVM.Transformers.SynthesisEngine
+{
+	public interface IAPICommand
+	{
+		String[] Users { get; set; }
+		void Execute();
+	}
+
+	public interface IChatCommand : IAPICommand
+	{
+		IChatHandler Handler { get; set; }
+		String TextMessage { get; set; }
+	}
+
+	public interface ILiveVideoCommand : IAPICommand
+	{
+	}
+
+	public interface ITransferFileCommand : IAPICommand
+	{
+		String FileLocation { get; set; }
+		IFileTransferHandler Handler { get; set; }
+	}
+
+	public interface IVoiceCallCommand : IAPICommand
+	{
+		ICallHandler Handler { get; set; }
+	}
+
+	public interface IEndCallCommand : IAPICommand
+	{
+	}
+}
--- trunk/RRComSSys/Projects/CVM/RRComSSys.CVM.Transformers/SynthesisEngine/Handlers.cs
+++ trunk/RRComSSys/Projects/CVM/RRComSSys.CVM.Transformers/SynthesisEngine/Handlers.cs
@@ -6,6 +6,16 @@ using SKYPE4COMLib;
 
 namespace RRComSSys.CVM.Transformers.SynthesisEngine
 {
+	public interface ICallHandler
+	{
+		void SetStatus(Call pCall, TCallStatus status);
+	}
+
+	public interface IChatHandler
+	{
+		void MessageReceived(ChatMessage message, TChatMessageStatus status);
+	}
+
 	public interface IFileTransferHandler
 	{
 		void SetStatus(IFileTransfer fileTransfer, TFileTransferStatus status);
